62392cff32d365a2069cbd5ddc2b5fd5880d7a75
Faith Nkirote <nkirotef054@gmail.com>
2025-09-19 22:28:54 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+  const paragraph = document.getElementById("text");
+  paragraph.textContent = "This is really cool!";
+  console.log("The DOM has loaded and the text has been changed");
+});
+
+console.log("This runs immediately when index.js loads");
